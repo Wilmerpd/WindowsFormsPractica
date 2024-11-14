7cb6c0253777ef7f8884dd62b8a8c16fc377dd88
lagar <lagar@WILMERPC>
2024-11-14 12:20:06 -0400

    Agregar archivos de proyecto.

---
+++ WindowsFormsPractica/Data/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsPractica.Modelo;
+
+namespace WindowsFormsPractica.Data
+{
+    public class Usuario
+    {
+        List<UsuarioModel> Lista = new List<UsuarioModel>();
+        //Guardar los usuarios
+        public void Guardar(UsuarioModel model)
+        {
+            Lista.Add(model);
+        }
+        //Consulta todos los usuarios
+        public List<UsuarioModel> Consultar()
+        {
+            return Lista;
+        }
+
+    }
+}
---
+++ WindowsFormsPractica/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsPractica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblNombre = new System.Windows.Forms.Label();
+            this.LblEdad = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.grilla = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grilla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LblNombre
+            // 
+            this.LblNombre.AutoSize = true;
+            this.LblNombre.Location = new System.Drawing.Point(138, 66);
+            this.LblNombre.Name = "LblNombre";
+            this.LblNombre.Size = new System.Drawing.Size(59, 16);
+            this.LblNombre.TabIndex = 0;
+            this.LblNombre.Text = "Nombre:";
+            // 
+            // LblEdad
+            // 
+            this.LblEdad.AutoSize = true;
+            this.LblEdad.Location = new System.Drawing.Point(138, 122);
+            this.LblEdad.Name = "LblEdad";
+            this.LblEdad.Size = new System.Drawing.Size(43, 16);
+            this.LblEdad.TabIndex = 1;
+            this.LblEdad.Text = "Edad:";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(207, 63);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(249, 22);
+            this.txtnombre.TabIndex = 2;
+            // 
+            // txtedad
+            // 
+            this.txtedad.Location = new System.Drawing.Point(207, 116);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(249, 22);
+            this.txtedad.TabIndex = 3;
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Location = new System.Drawing.Point(562, 47);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(113, 55);
+            this.btnguardar.TabIndex = 4;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Location = new System.Drawing.Point(562, 116);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(113, 52);
+            this.btnlimpiar.TabIndex = 5;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // grilla
+            // 
+            this.grilla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grilla.Location = new System.Drawing.Point(141, 181);
+            this.grilla.Name = "grilla";
+            this.grilla.RowHeadersWidth = 51;
+            this.grilla.RowTemplate.Height = 24;
+            this.grilla.Size = new System.Drawing.Size(442, 235);
+            this.grilla.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grilla);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.LblEdad);
+            this.Controls.Add(this.LblNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grilla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblNombre;
+        private System.Windows.Forms.Label LblEdad;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.DataGridView grilla;
+    }
+}
+
